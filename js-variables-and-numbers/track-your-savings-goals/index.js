27845060180711c347464fa44b53302f1cbec056
codebypaulina <paulinaczeschla@web.de>
2024-08-01 12:40:15 +0200

    solved track-your-savings-goals challenge

---
+++ js-variables-and-numbers/track-your-savings-goals/index.js
@@ -0,0 +1,11 @@
+const startingBalance = 1000;
+const monthlyContribution = 200;
+const months = 12;
+
+const totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After 12 months of saving, you will have " +
+    totalSavings +
+    " € in your account"
+);
