711a44b65d07e19a8f803e7510a3e66fa4324278
codebypaulina <paulinaczeschla@web.de>
2024-08-07 20:16:08 +0200

    solved handle-form-submit challenge

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+
+  const firstName = data.firstName;
+  const ageBadnessSum = Number(data.age) + Number(data.badness);
+
+  console.log(`The age-badness-sum of ${firstName} is ${ageBadnessSum}.`);
+
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
