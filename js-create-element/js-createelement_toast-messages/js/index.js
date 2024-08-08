4c25ece8126db4f35e8186433127100d4d1aa6c7
codebypaulina <paulinaczeschla@web.de>
2024-08-08 12:30:48 +0200

    solved toast-messages challenge

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,15 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToastMessage = document.createElement("li");
+  newToastMessage.className = "toast-container__message";
+  newToastMessage.textContent =
+    "WHAT'S UUUUUUUUUUUUP? I'M A NEW TOAST MESSAGE!";
+  toastContainer.append(newToastMessage);
+  return newToastMessage;
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
