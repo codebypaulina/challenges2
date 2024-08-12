23e70043f682e0c78009cd9e5235a671f9dc8b7e
codebypaulina <paulinaczeschla@web.de>
2024-08-12 20:29:10 +0200

    solved stars challenge

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,12 +2,26 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
+  for (let i = 1; i <= 5; i++) {
+    const imageElementStar = document.createElement("img");
 
+    imageElementStar.addEventListener("click", () => {
+      renderStars(i);
+    });
+
+    if (i <= filledStars) {
+      imageElementStar.src = "./assets/star-filled.svg";
+    } else {
+      imageElementStar.src = "./assets/star-empty.svg";
+    }
+
+    starContainer.append(imageElementStar);
+  }
   //--^-- your code here --^--
 }
 
