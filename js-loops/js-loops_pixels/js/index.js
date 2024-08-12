fbb75108f8d03435d00e96fcb31a96cdd13948cc
codebypaulina <paulinaczeschla@web.de>
2024-08-12 21:49:58 +0200

    solved pixels challenge

--- js-loops/js-loops_pixels/js/index.js
+++ js-loops/js-loops_pixels/js/index.js
@@ -123,3 +123,51 @@ const pixelValues = [
 ];
 
 const canvas = document.querySelector('[data-js="canvas"]');
+
+for (
+  let rowPixelValues = 0;
+  rowPixelValues < pixelValues.length;
+  rowPixelValues++
+) {
+  const currentRow = pixelValues[rowPixelValues];
+
+  for (
+    let columnPixelValues = 0;
+    columnPixelValues < currentRow.length;
+    columnPixelValues++
+  ) {
+    const colorPixel = currentRow[columnPixelValues];
+
+    const pixelElement = document.createElement("div");
+    pixelElement.classList.add("pixel");
+    pixelElement.style.backgroundColor = colorPixel;
+
+    canvas.append(pixelElement);
+  }
+}
+
+/********************** IN WORTEN: **********************
+ 
+for (
+  let rowPixelValues = 0; // Startpunkt der Loop = erste Zeile
+  rowPixelValues < pixelValues.length; // Loop wird ausgeführt, solange rowPixelValues kleiner ist als pixelValues.length: Loop wird so oft ausgeführt, wie es Zeilen in pixelValues gibt
+  rowPixelValues++ // bei jedem Durchlauf wird rowPixelValues auf den nächsten Zeilenindex gesetzt
+) {
+  const currentRow = pixelValues[rowPixelValues]; // aktuelle Zeile aus pixelValues-Array; jede Zeile wird durchlaufen
+
+  for (
+    let columnPixelValues = 0; // Startpunkt der Loop = erste Spalte in aktueller Zeile
+    columnPixelValues < currentRow.length; // Loop wird ausgeführt, solange columnPixelValues kleiner ist als currentRow.length: Loop wird so oft ausgeführt, wie es Spalten in der aktuellen Zeile gibt
+    columnPixelValues++ // bei jedem Durchlauf wird columnPixelValues auf den nächsten Spaltenindex gesetzt
+  ) {
+    const colorPixel = currentRow[columnPixelValues]; // Farbwert der aktuellen Spalte aus der aktuellen Zeile; jeder Farbwert in jeder Zeile wird durchlaufen
+
+    const pixelElement = document.createElement("div");
+    pixelElement.classList.add("pixel");
+    pixelElement.style.backgroundColor = colorPixel;
+
+    canvas.append(pixelElement);
+  }
+}
+
+********************************************************/
