9e38de4734ed707d8483a3c3594536593d5932f9
codebypaulina <paulinaczeschla@web.de>
2024-08-13 11:35:41 +0200

    solved callback-functions-with-multiple-arguments challenge

--- js-callback-functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ js-callback-functions/js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -4,4 +4,32 @@ function handleUserLogin(onSuccess) {
   onSuccess("Jane Doe", "admin");
 }
 
-// The exercise starts here!
+/* The exercise starts here!
+
+You should write a callback function in two different ways.
+In both cases your callback function should accept two parameters: "userName" and "userRole".
+The following message should be logged to the console:
+"Welcome ${userName}! You are logged in now as ${userRole}."
+
+1.  Write a function with the "function" keyword named "showWelcomeMessage".
+    Call "handleUserLogin" and pass your callback function as the first argument.
+    (You should see the log from your callback message) */
+
+function showWelcomeMessage(userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+}
+
+// handleUserLogin(showWelcomeMessage);
+
+/* 2.  Call "handleUserLogin" again but this time write the callback function as an anonymous function **inside** the function call (i.e. between the round brackets).
+       (You should see the log from your callback message) */
+
+handleUserLogin(function (userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+});
+
+// OR ARROW FUNCTION:
+
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`);
+});
