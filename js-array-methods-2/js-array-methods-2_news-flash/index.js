8c3a7c991f446221a5306c100dadd5391d9c8a33
codebypaulina <paulinaczeschla@web.de>
2024-08-15 15:58:59 +0200

    solved news-flash challenge

--- js-array-methods-2/js-array-methods-2_news-flash/index.js
+++ js-array-methods-2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,28 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+const filteredNews = news.filter((card) => {
+  return card.categories.includes("politics");
 });
 
+/* IMPLICIT RETURN:
+const filteredNews = news.filter((card) =>
+  card.categories.includes("politics")
+); */
+
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.sort((a, b) => a.body.length - b.body.length);
+
+/* LONG WAY:
+const sortedNews = filteredNews.sort((a, b) => {
+  if (a.body.length < b.body.length) {
+    return -1;
+  }
+  if (a.body.length > b.body.length) {
+    return 1;
+  }
+  return 0;
+}); */
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
