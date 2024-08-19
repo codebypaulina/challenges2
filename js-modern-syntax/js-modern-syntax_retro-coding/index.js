516f325290dcabe9a0b77d39d142ecf94ce599a5
codebypaulina <paulinaczeschla@web.de>
2024-08-19 19:54:50 +0200

    solved retro-coding challenge

--- js-modern-syntax/js-modern-syntax_retro-coding/index.js
+++ js-modern-syntax/js-modern-syntax_retro-coding/index.js
@@ -27,3 +27,41 @@ let getRelocatedCity = (
    - To combine objects or copy them, initiate a new object and loop through properties of the source object(s), copying each property explicitly.
 
  **************************************************************************************************************************/
+
+// Hint 1
+function getNameAndCountry(city) {
+  return [city.name, city.country]; // Hint 2
+}
+
+// Hint 1
+function getRelocatedCity(city1, city2) {
+  // Hint 3
+  if (city2 === undefined) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+
+  // Hint 4
+  const country = getNameAndCountry(city2)[1]; // access to second element of `city2`-object (`country`)
+  const relocatedCity = {}; // new empty object for copying the properties from `city1` & `country`
+
+  // copy all properties from `city1` to `relocatedCity` with FOR...IN
+  for (const key in city1) {
+    if (city1.hasOwnProperty(key)) {
+      relocatedCity[key] = city1[key]; // copy all properties from `city1` to `relocatedCity`
+    }
+  }
+
+  // add new `country` property
+  relocatedCity.country = country;
+
+  return relocatedCity;
+}
+
+// test
+const city1 = {
+  name: "Cologne",
+  state: "North Rhine-Westphalia",
+  country: "",
+  population: 1087353,
+};
+console.log(getRelocatedCity(city1));
