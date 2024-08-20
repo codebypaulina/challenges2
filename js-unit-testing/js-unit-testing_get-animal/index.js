cd4315e79a3d009d8576d37bca91b8a81799b60f
codebypaulina <paulinaczeschla@web.de>
2024-08-20 11:09:11 +0200

    solved animals challenge

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(animalPluralName) {
+  if (animalPluralName === "cats") {
+    return `I totally love cats!`;
+  }
+
+  if (animalPluralName) {
+    return `I like ${animalPluralName}!`;
+  }
+
+  return `I do not like animals at all!`;
+}
