8877bc71789804724a6fc31b36f8f0f296a4ac0e
codebypaulina <paulinaczeschla@web.de>
2024-08-23 15:41:46 +0200

    solved hello-world challenge

--- react-basics/react-basics_hello-world/src/App.jsx
+++ react-basics/react-basics_hello-world/src/App.jsx
@@ -1,5 +1,16 @@
 import "./App.css";
 
+/* Task 1: Render by Modifying the `App` component
 export default function App() {
-  return <div>Say hello...</div>;
+  return <h1>Hello world!</h1>;
+}
+*/
+
+// Task 2: Create Your First Custom Component
+export default function App() {
+  return <HelloWorld />;
+}
+
+export function HelloWorld() {
+  return <h1>Hello world!</h1>;
 }
