1d77bd5430d0506734423b9e9f159ad6450877b6
codebypaulina <paulinaczeschla@web.de>
2024-08-28 17:26:17 +0200

    solved personal-details-form challenge

--- react-state-2/react-state-2_personal-details/src/components/Form.jsx
+++ react-state-2/react-state-2_personal-details/src/components/Form.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
 import "./Form.css";
 
-
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form({ onCreateUser }) {
   function handleSubmit(event) {
     event.preventDefault();
+
+    // task 1, step 1: Extend the `handleSubmit` function to set the
+    // `name` and `email` state to its corresponding form field values.
+    const form = event.target;
+
+    // task 2: Refactor the `handleSubmit` function so that it calls `onCreateUser`
+    // with the correct values of the form fields as arguments.
+    onCreateUser(form.elements.name.value, form.elements.email.value);
+
+    // task 1, step 2: Reset the form after submit.
+    form.reset();
   }
 
   return (
@@ -18,9 +24,9 @@ export default function Form() {
     >
       <h2 id="user-details">Please enter your details here!</h2>
       <label htmlFor="name">Name: </label>
-      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <input id="name" name="name" type="text" placeholder="Your name" />
       <label htmlFor="email">Email: </label>
-      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <input id="email" name="email" type="email" placeholder="Your email" />
       <button className="form__submit-button" type="submit">
         Submit
       </button>
