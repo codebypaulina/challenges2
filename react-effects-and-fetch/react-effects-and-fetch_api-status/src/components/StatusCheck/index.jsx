7e80f5d6ee87382a1ea4901abc422fb39cf154bc
codebypaulina <paulinaczeschla@web.de>
2024-09-01 22:29:31 +0200

    solved api-status-and-error-handling challenge

--- react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
+++ react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
@@ -1,12 +1,13 @@
 import "./StatusCheck.css";
+import { useState } from "react";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
+  const [statusIcon, setStatusIcon] = useState("⁉️");
   // Something needs to change here…
   // ↙️
-  function handleCheckApiStatus() {
+  async function handleCheckApiStatus() {
     /**
      * Hint 1:
      * Use the `fetch()` function and pass the `apiStatusUrl` into it
@@ -20,8 +21,16 @@ export default function StatusCheck() {
      * is okay and false if it is not.
      **/
     // --v-- write your code here --v--
-    // --^-- write your code here --^--
+    setStatusIcon("⏳");
+    try {
+      const response = await fetch(apiStatusUrl);
+      response.ok ? setStatusIcon("✅") : setStatusIcon("❌");
+    } catch (error) {
+      console.log(error);
+      setStatusIcon("🚨");
+    }
   }
+  // --^-- write your code here --^--
 
   return (
     <article className="status-check">
