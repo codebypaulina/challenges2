717f80eb88b30eb1bfa1c18bc3900569c68c1c8a
codebypaulina <paulinaczeschla@web.de>
2024-09-04 19:08:38 +0200

    task: 'Detail Page Contents [with Bonus]'

--- nextjs-basics-and-routing/lotr-app/pages/volumes/the-return-of-the-king.js
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/the-return-of-the-king.js
@@ -2,25 +2,38 @@ import { volumes } from "@/resources/lib/data";
 import Link from "next/link";
 
 export default function TheReturnOfTheKing() {
-  const volume = volumes.find(
-    (volume) => volume.slug === "the-return-of-the-king"
+  const volumeSlug = volumes.findIndex(
+    ({ slug }) => slug === "the-return-of-the-king"
   );
+
+  const currentVolume = volumes[volumeSlug];
+  const nextVolume = volumes[volumeSlug + 1];
+  const previousVolume = volumes[volumeSlug - 1];
 
   return (
     <>
       <Link href="/volumes">← All Volumes</Link>
-      <h1>{volume.title}</h1>
-      <p>{volume.description}</p>
+      <h1>{currentVolume.title}</h1>
+      <p>{currentVolume.description}</p>
       <ul>
-        {volume.books.map(({ ordinal, title }) => {
+        {currentVolume.books.map(({ ordinal, title }) => {
           return (
-            <li key={ordinal}>
-              <p>{ordinal}</p>
-              <p>{title}</p>
+            <li key={volumeSlug}>
+              {ordinal}: <strong>{title}</strong>
             </li>
           );
         })}
       </ul>
+      {previousVolume ? (
+        <Link href={`/volumes/${previousVolume.slug}`}>
+          ← Previous Volume: {previousVolume.title}
+        </Link>
+      ) : null}
+      {nextVolume ? (
+        <Link href={`/volumes/${nextVolume.slug}`}>
+          Next Volume: {nextVolume.title} →
+        </Link>
+      ) : null}
     </>
   );
 }
--- nextjs-basics-and-routing/lotr-app/pages/volumes/the-two-towers.js
+++ nextjs-basics-and-routing/lotr-app/pages/volumes/the-two-towers.js
@@ -2,23 +2,37 @@ import { volumes } from "@/resources/lib/data";
 import Link from "next/link";
 
 export default function TheTwoTowers() {
-  const volume = volumes.find((volume) => volume.slug === "the-two-towers");
+  const volumeSlug = volumes.findIndex(({ slug }) => slug === "the-two-towers");
+
+  const currentVolume = volumes[volumeSlug];
+  const nextVolume = volumes[volumeSlug + 1];
+  const previousVolume = volumes[volumeSlug - 1];
 
   return (
     <>
       <Link href="/volumes">← All Volumes</Link>
-      <h1>{volume.title}</h1>
-      <p>{volume.description}</p>
+      <h1>{currentVolume.title}</h1>
+      <p>{currentVolume.description}</p>
       <ul>
-        {volume.books.map(({ ordinal, title }) => {
+        {currentVolume.books.map(({ ordinal, title }) => {
           return (
-            <li key={ordinal}>
-              <p>{ordinal}</p>
-              <p>{title}</p>
+            <li key={volumeSlug}>
+              {ordinal}: <strong>{title}</strong>
             </li>
           );
         })}
       </ul>
+      {previousVolume ? (
+        <Link href={`/volumes/${previousVolume.slug}`}>
+          ← Previous Volume: {previousVolume.title}
+        </Link>
+      ) : null}
+      <br />
+      {nextVolume ? (
+        <Link href={`/volumes/${nextVolume.slug}`}>
+          Next Volume: {nextVolume.title} →
+        </Link>
+      ) : null}
     </>
   );
 }
