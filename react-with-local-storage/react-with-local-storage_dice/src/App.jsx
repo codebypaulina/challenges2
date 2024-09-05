269d6b3844329d78629bd05df92d1c8d97e8138f
codebypaulina <paulinaczeschla@web.de>
2024-09-05 14:00:41 +0200

    solved dice challenge

--- react-with-local-storage/react-with-local-storage_dice/src/App.jsx
+++ react-with-local-storage/react-with-local-storage_dice/src/App.jsx
@@ -1,15 +1,29 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./App.css";
+import { getD6Roll } from "./utils";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+  // Zustand für Würfe (rolls)
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  // Funktion zum Hinzufügen eines neuen Wurfs
+  const handleRoll = () => {
+    const newRollValue = { value: getD6Roll(), time: Date.now() }; // neuen Wurf erstellen
+    setRolls([newRollValue, ...rolls]); // neuen Wurf an Anfang des Arrays setzen
+  };
+
+  // aktueller Wurfwert (erster Wert im "rolls"-Array)
+  const currentRollValue = rolls[0]?.value;
+
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button value={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
