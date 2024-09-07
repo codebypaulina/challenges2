2e4f15fd3b1b87dcd117254698a2db75af281c49
codebypaulina <paulinaczeschla@web.de>
2024-09-07 16:12:58 +0200

    solved smart-home-lights-control challenge

--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -1,11 +1,89 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initialLights = [
+  { id: "1", name: "Living Room", isOn: false },
+  { id: "2", name: "Kitchen", isOn: false },
+  { id: "3", name: "Bedroom", isOn: false },
+  { id: "4", name: "Bathroom", isOn: false },
+  { id: "5", name: "Garage", isOn: false },
+  { id: "6", name: "Porch", isOn: false },
+  { id: "7", name: "Garden", isOn: false },
+  { id: "8", name: "Office", isOn: false },
+];
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+  const isDimmed = lights.every((light) => !light.isOn);
+
+  function handleToggleIsOn(id) {
+    setLights(
+      lights.map((light) =>
+        light.id === id ? { ...light, isOn: !light.isOn } : light
+      )
+    );
+  }
+
+  function handleToggleAllLightsOn() {
+    setLights(
+      lights.map((light) => (!light.isOn ? { ...light, isOn: true } : light))
+    );
+  }
+
+  function handleToggleAllLightsOff() {
+    setLights(
+      lights.map((light) => (light.isOn ? { ...light, isOn: false } : light))
+    );
+  }
+
   return (
-    <Layout>
+    <Layout isDimmed={isDimmed}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        onToggleIsOn={handleToggleIsOn}
+        onToggleAllLightsOn={handleToggleAllLightsOn}
+        onToggleAllLightsOff={handleToggleAllLightsOff}
+      />
     </Layout>
   );
 }
+
+/************ FCTN WITH IF-STATEMENT ************
+ 
+  function handleToggleIsOn(id) {
+    setLights(
+      lights.map((light) => {
+        if (light.id === id) {
+          return { ...light, isOn: !light.isOn };
+        }
+        return light;
+      })
+    );
+  }
+    
+  function handleToggleAllLightsOn() {
+    setLights(
+      lights.map((light) => {
+        if (!light.isOn) {
+          return { ...light, isOn: true };
+        }
+        return light;
+      })
+    );
+  }
+    
+  function handleToggleAllLightsOff() {
+    setLights(
+      lights.map((light) => {
+        if (light.isOn) {
+          return { ...light, isOn: false };
+        }
+        return light;
+      })
+    );
+  }
+
+  *************************************************/
